4153b9671caf15bf5829cac1998025aed5d64c49
SATEJ SARKER <satejall@gmail.com>
2017-12-13 01:01:56 +0600

    done till 13  datetime fild

--- shortener/models.py
+++ shortener/models.py
@@ -5,6 +5,10 @@ from django.db import models
 # Create your models here.
 class KirrURL(models.Model):
     url=models.CharField(max_length=220)
+    shortcode=models.CharField(max_length=30,null=True,unique=True)
+    updated=models.DateTimeField(auto_now=True,null=True,blank=True)
+    timestamp=models.DateTimeField(auto_now_add=True,null=True,blank=True)
+
     def __str__(self):
         return str(self.url)
     def __unicode__(self):

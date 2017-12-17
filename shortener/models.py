4fa3e8b14b29b45748878c462e04eb6c91270cb8
keysarker@outlook.com <keysarker@outlook.com>
2017-12-17 19:25:09 +0600

    added random function and cheeking random

--- shortener/models.py
+++ shortener/models.py
@@ -1,13 +1,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from .util import code_generator
+
 
 # Create your models here.
 class KirrURL(models.Model):
     url=models.CharField(max_length=220)
-    shortcode=models.CharField(max_length=30,null=True,unique=True)
+    shortcode=models.CharField(max_length=30,blank=True,unique=True)
     updated=models.DateTimeField(auto_now=True,null=True,blank=True)
     timestamp=models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    
+    
+    def save(self,*args,**kwargs):
+       if self.shortcode is None or self.shortcode=="":
+           self.shortcode=code_generator()
+           
+       super(KirrURL,self).save(*args,**kwargs)
 
     def __str__(self):
         return str(self.url)
